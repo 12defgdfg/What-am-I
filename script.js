65c58fb3fb9c0b69c7e8cc060ee3958a55246462
12defgdfg <32374431+12defgdfg@users.noreply.github.com>
2019-04-11 17:47:24 -0400

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 $("#big").hide();
 $("#full").hide();
+$("#the").hide();
+$("#friend").hide();
 
 $("#click").click(function() {
-    var year = $("#birthYear").val();
+    var year = parseInt($("#birthYear").val());
     var firstInitial = $("#initial").val();
-    //console.log("N".charChodeAt(0) <= firstInitial.toUpperCase().charChodeAt(0));
-    //console.log(firstInitial.toUpperCase().charChodeAt(0)  <= "Z".charChodeAt(0));
+    console.log(parseInt($("#birthYear").val()));
+    console.log($("#birthYear").val());
     if (year === "" || firstInitial === "") {
 
-    } else if (year <= 1999 || firstInitial <= 15) {
-        $("#full").show();
+    } else if (year <= 1999 && "A".charCodeAt(0) <= firstInitial.toUpperCase().charCodeAt(0) && firstInitial.toUpperCase().charCodeAt(0) <= "M".charCodeAt(0)) {
         $("#big").hide();
-    } else if (year >= 2000 || "N".charChodeAt(0) <= firstInitial.toUpperCase().charChodeAt(0) && firstInitial.toUpperCase().charChodeAt(0)  <= "Z".charChodeAt(0)) {
+        $("#full").show();
+        $("#the").hide();
+        $("#friend").hide();
+        $("#result").text("You are Full House");
+        console.log(1);
+    } else if (year <= 1999 && "N".charCodeAt(0) <= firstInitial.toUpperCase().charCodeAt(0) && firstInitial.toUpperCase().charCodeAt(0) <= "Z".charCodeAt(0)) {
+        $("#big").hide();
+        $("#full").hide();
+        $("#the").show();
+        $("#friend").hide();
+        $("#result").text("You are Full House");
+    } else if (year >= 2000 && "A".charCodeAt(0) <= firstInitial.toUpperCase().charCodeAt(0) && firstInitial.toUpperCase().charCodeAt(0) <= "M".charCodeAt(0)) {
+        $("#big").hide();
+        $("#full").hide();
+        $("#the").hide();
+        $("#friend").show();
+    } else if (year >= 2000 && "N".charCodeAt(0) <= firstInitial.toUpperCase().charCodeAt(0) && firstInitial.toUpperCase().charCodeAt(0) <= "Z".charCodeAt(0)) {
         $("#big").show();
         $("#full").hide();
+        $("#the").hide();
+        $("#friend").hide();
     }
+
 });
